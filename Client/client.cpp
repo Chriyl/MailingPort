a4af7869fe44848bbf6aff8ab64c5ce3600e3db8
Christian <Christianmontanaro390@gmail.com>
2025-01-02 23:40:49 +0100

    init client

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "../_HeaderFiles/socket_MAN.hh"
+
+#define PORT 8080
+
 int main()
 {
-    return 0;
+
+    WSADATA wsa;
+
+    if (!SOCKETMAN::init_wsa(&wsa)) {
+        printf("[-] Errore con WSA\n");
+        return -1;
+    }
+
+    SOCKET sock = SOCKETMAN::init_sock();
+    if (sock == INVALID_SOCKET) {
+        return -1;
+    }
+
+    sockaddr_in server_addr;
+    SOCKETMAN::write_sockaddr(&server_addr, (short int)PORT);
 }
